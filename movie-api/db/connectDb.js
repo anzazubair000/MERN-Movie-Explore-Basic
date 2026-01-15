b458c7e0e3305e6888ea59e86729af42123d1d9e
anzazubair000 <162615606+anzazubair000@users.noreply.github.com>
2026-01-15 17:10:17 +0500

    Update connectDb.js
    simple string change

--- movie-api/db/connectDb.js
+++ movie-api/db/connectDb.js
@@ -3,7 +3,7 @@ import mongoose from "mongoose";
 const connectDb = async (Db_URL, Db_Name) => {
   try {
     await mongoose.connect(Db_URL, { dbName: Db_Name });
-    console.log(`✅ Database connection successful: ${Db_Name}`);
+    console.log(`✅ Database connection successfull: ${Db_Name}`);
   } catch (error) {
     console.log(`❌ Database connection failed: ${error.message}`);
   }
